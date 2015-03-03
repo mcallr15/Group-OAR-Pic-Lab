b4935ee587050f308c3f20b1b15b813f7545e4ce
mcallr15 <mcallr15@polyprep.org>
2015-03-03 09:51:22 -0500

    number 1 added

--- a8.java
+++ a8.java
@@ -1,1 +1,19 @@
+//1)
+public void copyParts(Picture fromPic, int startRow, int endRow, int startCol, int endCol)
+  {
+    Pixel fromPixel = null;
+    Pixel toPixel = null;
+    Pixel[][] toPixels = this.getPixels2D();
+    Pixel[][] fromPixels = fromPic.getPixels2D();
+    for (int fromRow = 0, toRow = startRow; fromRow < endRow && toRow < endRow; fromRow++, toRow++)
+    {
+      for (int fromCol = 0, toCol = startCol;fromCol < endCol && toCol < endCol;fromCol++, toCol++)
+      {
+        fromPixel = fromPixels[fromRow][fromCol];
+        toPixel = toPixels[toRow][toCol];
+        toPixel.setColor(fromPixel.getColor());
+      }
+    }   
+  }
 
+//2)
