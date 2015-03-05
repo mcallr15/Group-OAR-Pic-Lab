3cfec93dd1c1d2b2198749b7852b480808db50fc
mcallr15 <mcallr15@polyprep.org>
2015-03-05 08:15:08 -0500

    number 1 added

--- a9.java
+++ a9.java
@@ -1,1 +1,23 @@
-
+//1)
+public void edgeDetectionUpDown(int edgeDist)
+  {
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    Pixel[][] pixels = this.getPixels2D();
+    Color rightColor = null;
+    
+    for (int row = 0; row < pixels.length-1; row++)
+    {
+      for (int col = 0; col < pixels[0].length; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row+1][col];
+        rightColor = rightPixel.getColor();
+        if (leftPixel.colorDistance(rightColor) > 
+            edgeDist)
+          leftPixel.setColor(Color.BLACK);
+        else
+          leftPixel.setColor(Color.WHITE);
+      }
+    }
+  }
